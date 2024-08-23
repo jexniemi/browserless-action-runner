3acbd0da7ce6aba5bdbfeff4949f6bf662e44c5e
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-08-23 18:19:02 +0300

    disable puppeteer sandbox

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto('https://example.com');
   
