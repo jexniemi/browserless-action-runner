e5d51b7ecf30858f869d137dc910cce068aa00f0
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-08-23 18:01:12 +0300

    add try/catch

--- example/index.js
+++ example/index.js
@@ -1,14 +1,18 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-
-  const elementQuery = 'h1'
-  await page.waitForSelector(elementQuery);
-
-  const h1Text = await page.$eval(elementQuery, el => el.textContent);
-
-  console.log('<H1> text:', h1Text);
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+  
+    const elementQuery = 'h1'
+    await page.waitForSelector(elementQuery);
+  
+    const h1Text = await page.$eval(elementQuery, el => el.textContent);
+  
+    console.log('<H1> text:', h1Text);
+  } catch(e) {
+    console.log(e)
+  }
 })();
