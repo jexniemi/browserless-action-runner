9ba11f3cb9b5ce78c23bd081444b0b7bffee48b4
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-08-24 11:47:57 +0300

    Use browserless/chromium (#1)
    * test workflow
    
    * Remove unnecessary quote
    
    * test pptruser dir
    
    * attempt fix
    
    * attempt fix
    
    * Copy all of the files directly
    
    * Copy files
    
    * update node version
    
    * edit docker command
    
    * test
    
    * debug
    
    * update image
    
    * update image
    
    * add try/catch
    
    * test removing steps
    
    * Check ndoe v
    
    * test running everything in single step
    
    * print docker logs
    
    * remove flag
    
    * disable puppeteer sandbox
    
    * use browserless/chromium
    
    * Fix input
    
    * edit action
    
    * fix action
    
    * Take list of start_commands as an input
    
    * add shell
    
    * fix inputs reading
    
    * Fix inputs
    
    * exit process
    
    * Modify example
    
    * Do not stop explicitly stop Docker command

--- example/index.js
+++ example/index.js
@@ -1,14 +1,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
+  try {
+    const browser = await puppeteer.launch({ browserWSEndpoint: 'ws://localhost:3000' });
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+  
+    const elementQuery = 'h1'
+    await page.waitForSelector(elementQuery);
+  
+    const h1Text = await page.$eval(elementQuery, el => el.textContent);
+  
+    console.log('Scraping result: <H1> text:', h1Text);
 
-  const elementQuery = 'h1'
-  await page.waitForSelector(elementQuery);
-
-  const h1Text = await page.$eval(elementQuery, el => el.textContent);
-
-  console.log('<H1> text:', h1Text);
+    browser.close();
+  } catch(e) {
+    console.log(e)
+    process.exit(1)
+  }
 })();
